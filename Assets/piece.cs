82554e4e7239e79cf0e1710aef2be0ae5bfeb39d
yangyiming0516 <yangyiming0516@163.com>
2017-10-17 22:46:32 +0300

    chess complete

--- Assets/piece.cs
+++ Assets/piece.cs
@@ -8,12 +8,17 @@ public class piece : MonoBehaviour {
     float dest;
     bool lift;
     bool fall;
+    bool moving;
     public float speed;
+    Vector3 target;
+    public board B;
+    float delta = 0.01f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         lift = false;
         fall = false;
+        moving = false;
         height = gameObject.transform.position.y;
         speed = 5;
 	}
@@ -38,17 +43,38 @@ public class piece : MonoBehaviour {
             {
                 fall = false;
             }
+        if (moving)
+        {
+            if (Mathf.Abs(gameObject.transform.position.z-target.z)>delta || Mathf.Abs(gameObject.transform.position.x - target.x) > delta)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            }
+            else
+            {
+                moving = false;
+                B.remove_piece();
+                godown();
+                B.next_grid.temp_piece = gameObject;
+            }
+        }
 	}
 
     public void goup()
     {
-        lift = true;
         dest = height + 3;
+        lift = true;
     }
 
     public void godown()
     {
-        fall = true;
         dest = height;
+        fall = true;
+    }
+
+    public void moveto(Vector3 t)
+    {
+        target = t;
+        target.y = gameObject.transform.position.y;
+        moving = true;
     }
 }
